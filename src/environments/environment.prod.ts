fea282652bb8aed57d09f09908862e4bed74a0b9
Yuseth <yuseth28124@gmail.com>
2026-05-27 21:32:20 -0500

    fix(auth): actualizar Google Client ID correcto con front-neo autorizado

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://backend-neogamin-production-58ed.up.railway.app',
-  googleClientId: '269227414737-jcq89d207le7pl29vri82g5c1ulfktfq.apps.googleusercontent.com',
+  googleClientId: '269227414737-j36bf7hvmn21h0vgmn78ipbvsbeh0du4.apps.googleusercontent.com',
 };
